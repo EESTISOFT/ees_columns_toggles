f6642d88ffdd190d50547aa118119547c751015e
giulio milani <giulpiomilani@gmail.com>
2019-02-11 01:29:46 +0100

    Update __manifest__.py

--- ees_columns_toggles/__manifest__.py
+++ ees_columns_toggles/__manifest__.py
@@ -6,6 +6,7 @@
     'website': 'https://www.eestisoft.com',
     'sequence': 2,
     'summary': 'Allows to toggle visibility of columns for every treeview in odoo',
+	'images':['static/description/thumbs.png'],
     'description': """
 
 Columns toggles for treeview
